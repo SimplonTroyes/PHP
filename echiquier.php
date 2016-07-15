0ca00392db18e603b120e06dcf2c976bae86f439
SimplonTroyes <xdarras@simplon.co>
2016-07-15 16:50:03 +0200

    bonus echiquier

---
+++ echiquier.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/styles.css" media="screen" title="no title" charset="utf-8">
+    <title>Echiquier</title>
+  </head>
+  <body>
+    <p>
+      Echiquier
+    </p>
+    <?php
+    for ($i=1;$i <= 8;$i++){
+      // Affichage d'une ligne
+      echo ' ';
+    }
+
+
+
+    ?>
+  </body>
+</html>
