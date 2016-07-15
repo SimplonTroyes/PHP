b335d55c43219d8dee79d3f1dbb57b71aaefb924
SimplonTroyes <xdarras@simplon.co>
2016-07-15 10:41:23 +0200

    ajout index php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Première page en PHP</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/styles.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    Comment écire en php ?<br>
+
+  </body>
+</html>
