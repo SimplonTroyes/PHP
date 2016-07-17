4eb5c3964c3ba085c602ba5002c5773ae35a409c
josselynh <josselynh@yahoo.com>
2016-07-17 09:07:29 +0200

    echiquier
    echiquier 1

--- echiquier.php
+++ echiquier.php
@@ -4,20 +4,27 @@
     <meta charset="utf-8">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/styles.css" media="screen" title="no title" charset="utf-8">
-    <title>Echiquier</title>
+    <title>Boucles en php</title>
   </head>
   <body>
+    Les boucles permettent de répeter plusieurs fois un même bloc d'instructions
     <p>
-      Echiquier
+      3 types de boucles :
+      <ul>
+        <li>Boucle    for .... (pour)</li>
+        <li>Boucle    while... (tant que)</li>
+        <li>Boucle    Do.... while (répéter jusqu'à)</li>
+      </ul>
     </p>
-    <?php
-    for ($i=1;$i <= 8;$i++){
-      // Affichage d'une ligne
-      echo ' ';
-    }
-
-
-
+    <p>
+      Echiquier <br>
+      <?php
+      for ($i=1;$i <= 8;$i++){
+        // Affichage d'une ligne
+        echo $i.' ';
+     }
     ?>
+    </p>
+
   </body>
 </html>
