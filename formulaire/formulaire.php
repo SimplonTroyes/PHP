0c22ee5611216c2603b10a88f0d613aadb70f4f2
SimplonTroyes <xdarras@simplon.co>
2016-07-19 08:34:59 +0200

    formulaire

---
+++ formulaire/formulaire.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Formulaire - Etape 1</title>
+  </head>
+  <body>
+    <form class="" action="trait_form_get.php" method="get">
+      Nom : <input type="text" name="nom"><br>
+      Prenom : <input type="text" name="prenom"><br>
+      <input type="submit" name="name" value="OK">
+    </form>
+  </body>
+</html>
